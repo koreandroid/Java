6702b0103cbaac066e7e2266d079c50184514ae6
koreandroid <dlwotjd116@snu.ac.kr>
2026-05-30 18:58:19 +0900

    Update Solution.java

--- 대충 만든 자판/Solution.java
+++ 대충 만든 자판/Solution.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class Solution {
 
     public int[] solution(String[] keymap, String[] targets) {
-        int[] pushCount = new int[26];
-        Arrays.fill(pushCount, Integer.MAX_VALUE);
+        int[] pushCounts = new int[26];
+        Arrays.fill(pushCounts, Integer.MAX_VALUE);
 
         for (String km : keymap) {
             for (int i = 0; i < km.length(); i++) {
-                if (i + 1 < pushCount[km.charAt(i) - 'A']) {
-                    pushCount[km.charAt(i) - 'A'] = i + 1;
+                if (i + 1 < pushCounts[km.charAt(i) - 'A']) {
+                    pushCounts[km.charAt(i) - 'A'] = i + 1;
                 }
             }
         }
@@ -17,12 +17,12 @@ class Solution {
         int[] totalCounts = new int[targets.length];
         for (int i = 0; i < targets.length; i++) {
             for (char ch : targets[i].toCharArray()) {
-                if (pushCount[ch - 'A'] == Integer.MAX_VALUE) {
+                if (pushCounts[ch - 'A'] == Integer.MAX_VALUE) {
                     totalCounts[i] = -1;
                     break;
                 }
 
-                totalCounts[i] += pushCount[ch - 'A'];
+                totalCounts[i] += pushCounts[ch - 'A'];
             }
         }
 
